48d206874dbaa4ec6bba98d75848cee5cd1e7d78
Chasty <coderessence@gmail.com>
2024-01-24 10:30:05 -0500

    feat: add parent component adder extension

---
+++ README.md
@@ -0,0 +1,54 @@
+# addparentcomponent README
+
+This extensions helps you to add a parent component and you can give any name.
+
+## Features
+
+Select a range of code and right-click to see the option `Àdd Parent Component` in the context menu. Click on it and you will be prompted to enter the name of the parent component. Enter the name and click on `OK`. The parent component will be created and the selected code will be moved to the parent component.
+
+## Requirements
+
+Install this extension from the VS Code Marketplace.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of addparentcomponent
+
+---
+
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
+
+- [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
+## Working with Markdown
+
+You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
+
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
+- Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
+- Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
+
+## For more information
+
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,111 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "addparentcomponent" is now active!'
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.addParentComponent",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      // vscode.window.showInformationMessage("Hello VS Code!");
+      addParentComponent();
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  registerContextMenu();
+}
+
+async function addParentComponent(customName?: string) {
+  const editor = vscode.window.activeTextEditor;
+
+  if (!editor) {
+    return;
+  }
+
+  const selection = editor.selection;
+  const selectedText = editor.document.getText(selection);
+
+  if (!selectedText) {
+    return;
+  }
+
+  const parentComponentName =
+    customName ||
+    (await vscode.window.showInputBox({
+      prompt: "Enter a name for the parent component:",
+      placeHolder: "AddedParent",
+      validateInput: (value) => (value.trim() ? null : "Name cannot be empty"),
+    }));
+
+  if (!parentComponentName) {
+    return;
+  }
+
+  const addedCode = `<${parentComponentName}>\n${selectedText}\n</${parentComponentName}>`;
+
+  editor.edit((editBuilder) => {
+    editBuilder.replace(selection, addedCode);
+  });
+}
+
+function registerContextMenu() {
+  // Register the command for context menu
+  vscode.commands.registerTextEditorCommand(
+    "extension.addParentComponentContextMenu",
+    (editor, edit, args) => {
+      const customName = args?.customName || "AddedParent"; // Default to 'AddedParent' if not provided
+      addParentComponent(customName);
+    }
+  );
+
+  // Register the context menu
+  vscode.window.onDidChangeTextEditorSelection((e) => {
+    updateContextMenu(e as unknown as vscode.TextEditor);
+  });
+
+  // Initial registration of the context menu
+  updateContextMenu(vscode.window.activeTextEditor);
+}
+
+function updateContextMenu(editor: vscode.TextEditor | undefined) {
+  if (editor) {
+    const command = editor.selection.isEmpty
+      ? undefined
+      : {
+          command: "extension.addParentComponentContextMenu",
+          title: "Add Parent Component",
+          arguments: [{ customName: "AddedParent" }],
+        };
+
+    vscode.commands.executeCommand(
+      "setContext",
+      "editorHasSelection",
+      !editor.selection.isEmpty
+    );
+
+    vscode.commands.executeCommand(
+      "setContext",
+      "editorContextMenuCommand",
+      command
+    );
+
+    vscode.window.showInformationMessage("Context menu updated");
+  }
+}
